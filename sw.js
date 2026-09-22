726010b338b4295fa99b0e827ddd3182ffe253ca
labiiknu <laboratoriumiiknu@gmail.com>
2026-09-22 13:12:33 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * diinstal dan tetap terbuka saat koneksi lemah. Isi SILABNU sendiri berasal
  * dari Apps Script dan selalu dimuat langsung dari server, tidak disimpan di sini.
  */
-const VERSI_CACHE = 'silabnu-v7';
+const VERSI_CACHE = 'silabnu-v8';
 const BERKAS_INTI = [
   './',
   './index.html',
